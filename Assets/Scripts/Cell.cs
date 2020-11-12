cb6d1d5908c9264e7a524f22bd56f0daa46859ad
iliaakm <iliaakm@gmail.com>
2020-11-12 19:57:19 +0300

    add cell

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum CellType
+{
+    Water, Land
+}
+
+public class Cell : MonoBehaviour
+{
+    public CellType _cellType { get; private set; }
+
+    Color _colorWater = Color.blue;
+    Color _colorLand = Color.green;
+
+    public void Generate(CellType type)
+    {
+        _cellType = type;
+
+        Color color = _colorWater;
+        if (type == CellType.Land) color = _colorLand;
+        GetComponent<Image>().color = color;
+    }
+}
